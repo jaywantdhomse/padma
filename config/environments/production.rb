4add2a6ff1cd129d13f28b885d7d75a26efe4e10
Rupesh Raundal <rupesh@zerebral.co.in>
2016-04-03 15:16:55 +0530

    gui changes and production conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,16 @@ Rails.application.configure do
   
   
   config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { :host => "http://padma-nsk.herokuapp.com/" }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'mail.gmail.com',
-  :user_name            => 'suvidhinathpadma@gmail.com',
-  :password             => 'padma123456',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "seplworld.in" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'mail.gmail.com',
+    :user_name            => 'suvidhinathpadma@gmail.com',
+    :password             => 'padma123456',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 end
